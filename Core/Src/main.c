fc1a79fb9fc800d5e967657532807f03bbe7980b
Stefan Falkenstein <sjfalken@gmail.com>
2021-06-06 12:12:07 -0400

    storing data in fifo

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,17 +1,30 @@
 
 #include "main.h"
 #define FFT_SIZE 1024
+#define ADC_FIFO_CHUNK_SIZE 512
+#define ADC_FIFO_NUM_CHUNKS 8
+#define ADC_FIFO_NUM_SAMPLES (ADC_FIFO_NUM_CHUNKS*ADC_FIFO_CHUNK_SIZE)
+#define MAX_DB 80
+#define SAMPLING_RATE 45000
+#define TIM_CLK_FREQ 90000000
 
 ADC_HandleTypeDef hadc1;
 DMA_HandleTypeDef hdma_adc1;
 TIM_HandleTypeDef htim2, htim3;
 UART_HandleTypeDef huart2;
 
-enum {
-  PROCESSING,
-  READY0,
-  READY1
-} fft_status;
+typedef uint16_t adc_sample_t;
+typedef union {
+  adc_sample_t samples[ADC_FIFO_NUM_SAMPLES];
+  adc_sample_t chunks[ADC_FIFO_NUM_CHUNKS][ADC_FIFO_CHUNK_SIZE];
+} adc_data_t;
+
+typedef struct {
+  adc_data_t data;
+  size_t head_chunk;
+} adc_fifo_t;
+
+volatile adc_fifo_t adc_fifo;
 
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
@@ -21,28 +34,29 @@ static void MX_TIM2_Init(void);
 static void MX_TIM3_Init(void);
 static void MX_USART2_UART_Init(void);
 
+static void ADC_FIFO_Init(void) {
+  adc_fifo.head_chunk = 0;
+}
+
 void DMA2_Stream0_IRQHandler(void) {
   HAL_DMA_IRQHandler(&hdma_adc1);
-
 }
 
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc) {
-  // if(() == RESET)
-  switch (fft_status) {
-    case PROCESSING:
-      if ((hadc->DMA_Handle->Instance->CR & DMA_SxCR_CT) == RESET)
-        fft_status = READY1;
-      else
-        fft_status = READY0;
-      break;
-    default:
-      fft_status = PROCESSING;
-      break;
+  if ((hadc->DMA_Handle->Instance->CR & DMA_SxCR_CT) == RESET) {
+    // if buf 1 is complete, update its address
+    hadc->DMA_Handle->Instance->M1AR = (uint32_t) adc_fifo.data.chunks[adc_fifo.head_chunk];
+  } else {
+    // else buf 0 is complete, update its address
+    hadc->DMA_Handle->Instance->M0AR = (uint32_t) adc_fifo.data.chunks[adc_fifo.head_chunk];
   }
+
+  adc_fifo.head_chunk = (adc_fifo.head_chunk + 1) % ADC_FIFO_NUM_CHUNKS;
+
 }
 
 int main(void) {
-  fft_status = PROCESSING;
+  // fft_status = PROCESSING;
   HAL_Init();
   SystemClock_Config();
 
@@ -53,65 +67,66 @@ int main(void) {
   MX_TIM3_Init();
   MX_USART2_UART_Init();
 
-  uint32_t adc_data[2][FFT_SIZE] = {0};
-  float32_t sample_buf[FFT_SIZE];
+  ADC_FIFO_Init();
+
+  // uint16_t adc_data[2][FFT_SIZE] = {0};
+  float32_t fft_in[FFT_SIZE];
   float32_t fft_out[FFT_SIZE];
-  float32_t uart_buf[FFT_SIZE/2];
+  float32_t sample_mag_db[FFT_SIZE/2];
 
   HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
   HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
-  HAL_ADC_Start_DoubleBuffer_DMA(&hadc1, adc_data[0], adc_data[1], FFT_SIZE);
+  HAL_ADC_Start_DoubleBuffer_DMA(&hadc1, (uint32_t*) adc_fifo.data.chunks[0], (uint32_t*) adc_fifo.data.chunks[0], ADC_FIFO_CHUNK_SIZE);
 
   arm_rfft_fast_instance_f32 fft_struct;
   arm_rfft_fast_init_f32(&fft_struct, FFT_SIZE);
 
   while (1)
   {
-    while (fft_status == PROCESSING) {
-      // wait
-    }
+    static size_t last_head_chunk = 0;
 
-    uint8_t buff_num = (fft_status == READY0) ? 0 : 1;
+    // wait for fifo to update
+    while (last_head_chunk == adc_fifo.head_chunk);
+    last_head_chunk = adc_fifo.head_chunk;
+
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);
 
     for (uint32_t i = 0; i < FFT_SIZE; i += 1) {
-      sample_buf[i] = ((float32_t) adc_data[buff_num][i] - (2048)) / (1 << 12);
+      size_t sample_idx = (adc_fifo.head_chunk*ADC_FIFO_CHUNK_SIZE + i) % ADC_FIFO_NUM_SAMPLES;
+
+      fft_in[i] = ((float32_t) adc_fifo.data.samples[sample_idx] - (2048)) / (1 << 12);
 
       // applying window
       float32_t a0 = 25/46;
-      sample_buf[i] = (a0 - (1-a0)*arm_cos_f32(2*PI*i/FFT_SIZE)) * sample_buf[i];
+      fft_in[i] = (a0 - (1-a0)*arm_cos_f32(2*PI*i/FFT_SIZE)) * fft_in[i];
     }
-    // sample_buf[0] = 0;
 
-    arm_rfft_fast_f32(&fft_struct, sample_buf, fft_out, 0);
-
-    float32_t max = 0.0;
+    arm_rfft_fast_f32(&fft_struct, fft_in, fft_out, 0);
 
     // suppress DC component
-    fft_out[0] = __FLT_EPSILON__;
-    fft_out[1] = __FLT_EPSILON__;
+    fft_out[0] = 0.0;
+    fft_out[1] = 0.0;
+    fft_out[2] = 0.0;
+    fft_out[3] = 0.0;
 
+    float32_t max = 0.0;
     for (uint32_t i = 0; i < FFT_SIZE/2; i++) {
       float32_t sample_raw[2] = {fft_out[i*2], fft_out[i*2+1]};
-      float32_t sample_temp, sample_mag_db;
-      arm_power_f32(sample_raw, 2, &sample_temp);
-      sample_mag_db = 10*log10f((float) sample_temp + __FLT_EPSILON__);
-      uart_buf[i] = sample_mag_db;
+      float32_t sample_mag_sq;
+      arm_power_f32(sample_raw, 2, &sample_mag_sq);
+      sample_mag_db[i] = 10*log10f((float) sample_mag_sq + __FLT_EPSILON__);
 
-      if (uart_buf[i] > max)
-        max = uart_buf[i];
-
+      if (sample_mag_db[i] > max) {
+        max = sample_mag_db[i];
+      }
     }
 
-    float32_t max_db = 80;
-    htim3.Instance->CCR2 = (uint32_t) ((max / max_db) * 1000.0);
-    // HAL_Delay((max + min) / __FLT_MAX__);
+    // update led pwm
+    htim3.Instance->CCR2 = (uint32_t) ((max / MAX_DB) * htim3.Instance->ARR);
+
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
     // HAL_UART_Transmit(&huart2, (uint8_t*) uart_buf, sizeof(float32_t)*FFT_SIZE/2, 0xFFFFFFFF);
-
-    // HAL_Delay(1);
-
-    fft_status = PROCESSING;
   }
-  /* USER CODE END 3 */
 }
 
 /**
@@ -231,9 +246,9 @@ static void MX_TIM2_Init(void)
 
   /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 200;
+  htim2.Init.Prescaler = 0;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 10;
+  htim2.Init.Period = TIM_CLK_FREQ / SAMPLING_RATE;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
@@ -256,7 +271,7 @@ static void MX_TIM2_Init(void)
     Error_Handler();
   }
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 1;
+  sConfigOC.Pulse = htim2.Init.Period / 100;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
   if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
@@ -279,9 +294,9 @@ static void MX_TIM3_Init(void)
   TIM_OC_InitTypeDef sConfigOC = {0};
 
   htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 2;
+  htim3.Init.Prescaler = 0;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 1000;
+  htim3.Init.Period = 2000;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
@@ -398,12 +413,10 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
 
   GPIO_InitStruct.Pin = GPIO_PIN_6;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  GPIO_InitStruct.Alternate = GPIO_AF2_TIM3;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
 }
 
 /* USER CODE BEGIN 4 */
